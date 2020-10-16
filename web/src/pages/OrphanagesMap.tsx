52276f1b480d1e4086579bfc3ff3c64cfd158132
Pier Guinzani <pier_guinzani@hotmail.com>
2020-10-15 22:12:38 -0300

    💄 feat:  Added map in orphanages page.

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
-
+import { Map, TileLayer } from "react-leaflet";
 
 import mapMarkerImg from "../images/map-marker.svg"
 import "../styles/pages/orphanages-map.css";
+import "leaflet/dist/leaflet.css"
 
 function OrphanagesMap() {
   return (
@@ -22,6 +23,14 @@ function OrphanagesMap() {
           <span>Maranhão</span>
         </footer>
       </aside>
+
+      <Map
+        center={[-2.533538,-44.1815083]}
+        zoom={15}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="FFF" />
