34529f591a56e65ae9124bffce1df182dbc8e274
Pier Guinzani <pier_guinzani@hotmail.com>
2020-10-24 09:55:27 -0300

    🗃  chore: Added upload images in images table database

--- backend/src/models/Orphanage.ts
+++ backend/src/models/Orphanage.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany, JoinColumn } from 'typeorm';
+import Image from './Image';
 
 @Entity('orphanages')
 export default class Orphanage {
@@ -7,17 +8,30 @@ export default class Orphanage {
 
   @Column()
   name: string;
+
   @Column()
   latitude: number;
+
   @Column()
   longitude: number;
+
   @Column()
   about: string;
+
   @Column()
   instructions: string;
+
   @Column()
   opening_hours: string;
+
   @Column()
   open_on_weekends: boolean;
+
+  @OneToMany(() => Image, image => image.orphanage, {
+    cascade: ['insert', 'update']
+  })
+  
+  @JoinColumn({ name: 'orphanage_id' })
+  images: Image[];
 
 }
